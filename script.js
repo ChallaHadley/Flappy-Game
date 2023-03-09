59eeac717e419ca79a5c9ca6e0294a3def017c3f
Challa Hadley <70288921+ChallaHadley@users.noreply.github.com>
2023-03-09 11:03:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+import { updateBird, setupBird, getBirdRect } from "./Bird.js"
+
+
+document.addEventListener("keypress", handleStart, {once: true})
+const title = document.querySelector("[data-title]")
+const subtitle = document.querySelector("[data-subtitle]")
+
+let lastTime
+function updateLoop(time){
+    if(lastTime == null){
+        lastTime = time
+        window.requestAnimationFrame(updateLoop)
+        return
+    }
+    const delta = time - lastTime
+    updateBird(delta)
+    if(checkLose()) return handleLose()
+    lastTime = time
+    window.requestAnimationFrame(updateLoop)
+}
+
+function checkLose(){
+    const birdRect = getBirdRect()
+
+    const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight
+    return outsideWorld
+}
+
+function handleStart(){
+    title.classList.add("hide")
+    setupBird()
+    lastTime = null
+    window.requestAnimationFrame(updateLoop)
+}
+
+function handleLose(){
+    setTimeout(() => {
+        title.classList.remove("hide")
+        subtitle.classList.remove("hide")
+        subtitle.textContent = "0 Pipes"
+
+        document.addEventListener("keypress", handleStart, {once: true})
+        
+    }, 100);
+    
+}
+
+//22:15, https://www.youtube.com/watch?v=Jgst0rihJ3o
